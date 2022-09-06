fb46f8d3d0b1056c33d2812e327ee8f4423aff65
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-09-06 15:12:27 -0500

    feat: se importaron elementos de swagger

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const morgan = require ('morgan');
 const infoServicesRoutes = require ('./routes/InfoServices.routes.js');
+const path = require("path");
 
 /*swagger */
-
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerSpec= {
+    definition: {
+        openapi : "3.0.0",
+        info : {
+            title: "Servicios Psicológicos México API",
+            version : "1.0.0"
+        },
+        servers : [
+            {
+                url : "http://localhost:5000"
+            }
+        ]
+    },
+    apis : [`${path.join(__dirname, "./routes/*.js")}`]
+}
 
 //settings
 const app = express();
@@ -13,6 +30,7 @@ app.set('port',port);
 /*Milddwares*/
 app.use(morgan("dev"));
 app.use(express.json());
+app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(swaggerJsdoc(swaggerSpec)))
 
 //route
 app.get('/', (req, res)=>{
